4a814daf54408afd1950132ae3f0c0ee57e53ebe
Unknown <veera.tikkamaki@gmail.com>
2019-04-14 18:11:46 +0300

    "Initial commit" with a lot of stuff

---
+++ Assets/Scripts/ControlScheme.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ControlScheme : ScriptableObject
+{
+    public GivenInput LeftStickMove;
+    public GivenInput RightStickMove;
+    public GivenInput LeftStickPress;
+    public GivenInput RightStickPress;
+    public GivenInput DPadUp;
+    public GivenInput DPadDown;
+    public GivenInput DPadLeft;
+    public GivenInput DPadRight;
+    public GivenInput FaceButtonUp;
+    public GivenInput FaceButtonDown;
+    public GivenInput FaceButtonLeft;
+    public GivenInput FaceButtonRight;
+    public GivenInput BumberLeft;
+    public GivenInput BumberRight;
+    public GivenInput TriggerLeft;
+    public GivenInput TriggerRight;
+    public GivenInput Back;
+    public GivenInput Start;
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    private void OnLStickMoved() {
+        DebugInput("LeftStickMove");
+    }
+
+    private void OnRStickMoved() {
+        DebugInput("RightStickMove");
+    }
+
+    private void OnLStickPressed() {
+        DebugInput("LeftStickPress");
+    }
+
+    private void OnRStickPressed() {
+        DebugInput("RightStickPress");
+    }
+
+    private void OnDPadUpPressed() {
+        DebugInput("DPadUp");
+    }
+
+    private void OnDPadDownPressed() {
+        DebugInput("DPadDown");
+    }
+
+    private void OnDPadLeftPressed() {
+        DebugInput("DPapLeft");
+    }
+
+    private void OnDPadRightPressed() {
+        DebugInput("DPadRight");
+    }
+
+    private void OnFaceButtonUpPressed() {
+        DebugInput("FaceButtonUp");
+    }
+
+    private void OnFaceButtonDownPressed() {
+        DebugInput("FaceButtonDown");
+    }
+
+    private void OnFaceButtonLeftPressed() {
+        DebugInput("FaceButtonLeft");
+    }
+
+    private void OnFaceButtonRightPressed() {
+        DebugInput("FaceButtonRight");
+    }
+
+    private void OnBumperLeftPressed() {
+        DebugInput("BumberLeft");
+    }
+
+    private void OnBumperRightPressed() {
+        DebugInput("BumberRight");
+    }
+
+    private void OnTriggerLeftPressed() {
+        DebugInput("TriggerLeft");
+    }
+
+    private void OnTriggerRightPressed() {
+        DebugInput("TriggerRight");
+    }
+
+    private void OnBackPressed() {
+        DebugInput("Back");
+    }
+
+    private void OnStartPressed() {
+        DebugInput("Start");
+    }
+
+    private void DebugInput(string input) {
+        Debug.Log(input);
+    }
+}
